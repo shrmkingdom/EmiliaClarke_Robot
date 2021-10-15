d3232f45167d052d37d158d5df9db6bd1cf2a3a4
Ragnarok360 <87938973+Ragnarok360@users.noreply.github.com>
2021-10-15 09:43:58 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -121,7 +121,7 @@ async def start(bot, message):
             )
         )
     else:
-        await message.send_photo(
+        await message.reply_photo(
             photo=f"{random.choice(PHOTO)}",
             caption=START_MSG,
             parse_mode="Markdown",
