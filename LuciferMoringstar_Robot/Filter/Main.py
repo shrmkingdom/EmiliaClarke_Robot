e6e68615fe08d99d7a657f9c0715ea384a3d63e8
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-10-16 21:47:44 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🎬 Title: {search}**\n**⭐ Rating: {random.choice(RATING)}**\n**🎭 Genre: {random.choice(GENRES)}**\n**©️ Group: {message.chat.title}🍿**\n**⭕ Requested By:** {cmd.from_user.mention}"
+        mo_tech_yt = f"**🎬 Title: {search}**\n**⭐ Rating: {random.choice(RATING)}**\n**🎭 Genre: {random.choice(GENRES)}**\n**©️ Group: {message.chat.title}🍿**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
