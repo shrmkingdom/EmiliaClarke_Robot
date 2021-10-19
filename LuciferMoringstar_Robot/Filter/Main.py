867e2b393f997b60262f562efe230e19e94024b7
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-10-19 13:33:02 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -7,6 +7,7 @@ from pyrogram.errors import UserNotParticipant
 from LuciferMoringstar_Robot import get_filter_results, get_file_details, is_subscribed, get_poster
 from LuciferMoringstar_Robot import RATING, GENRES, HELP, ABOUT
 import random
+import asyncio
 BUTTONS = {}
 BOT = {}
 
