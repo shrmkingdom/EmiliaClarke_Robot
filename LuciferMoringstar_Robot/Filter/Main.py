d9f2cfcf78160ce66fc96190a46460556efb8397
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-10-24 09:44:38 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -144,11 +144,7 @@ async def group(client, message):
             LuciferMoringstar=await client.send_photo(
             chat_id = message.chat.id,
             photo="https://telegra.ph/file/7df87dc1504e251d084de.jpg",
- caption="<b>Couldn't Find This Movie.Try Again..! Check The Spelling Is Correct . 
-
-Or  🙏Please Check That Other Bots Will Provided The Movie That You Request
-
- Or  Admins will add it Soon 🔜🥺</b>",
+            caption="<b>Couldn't Find This Movie.Try Again..! Check The Spelling Is Correct.\n Or  🙏Please Check That Other Bots Will Provided The Movie That You Request.\n Or  Admins will add it Soon 🔜🥺</b>",
             parse_mode="html",
             reply_to_message_id=message.message_id
         )
